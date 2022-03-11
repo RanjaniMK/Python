5771a0470ba786b7f3b63902c7fef52783eefc52
“ranjani <ranjani.murugesankarunagaran@stud.tu-darmstadt.de>
2022-03-11 09:22:42 +0100

    FileIO

---
+++ FileRead.py
@@ -0,0 +1,4 @@
+my_file=open('Content.txt')
+print(my_file.readlines())
+
+my_file.close()
---
+++ FileReadAgain.py
@@ -0,0 +1,3 @@
+with open('sad.txt', mode='w') as f1:
+    writing=f1.write("sad feeling")
+    print(writing)
